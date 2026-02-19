f11e96f5eb447a44ebf2daa409fe1248163e57d5
James Shi <mrjamesshi@gmail.com>
2026-02-19 21:18:47 +0000

    File changes

--- src/pages/Consultant.jsx
+++ src/pages/Consultant.jsx
@@ -775,6 +775,12 @@ Return empty array if user didn't provide any of these facts.`;
                 key={index}
                 message={msg}
                 isUser={msg.role === 'user'}
+                onViewSchoolProfile={(slug) => {
+                  const school = schools.find(s => s.slug === slug);
+                  if (school) {
+                    handleViewSchoolDetail(school);
+                  }
+                }}
               />
             ))}
             {isTyping && <TypingIndicator message={thinkingMessage} />}
