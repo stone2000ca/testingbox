71fa175653efb30c9d3e29a4343fe62183127035
James Shi <mrjamesshi@gmail.com>
2026-02-19 21:53:58 +0000

    File changes

--- src/components/schools/SchoolGrid.jsx
+++ src/components/schools/SchoolGrid.jsx
@@ -10,7 +10,7 @@ export default function SchoolGrid({ schools, onViewDetails, onToggleShortlist, 
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
       {schools.map((school, index) => (
         <SchoolCard
           key={school.id}
